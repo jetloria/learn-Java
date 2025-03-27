ddb207b4445ac92191b127ed1214d1f2dfbdc4da
jetloria <jethroloria25@gmail.com>
2025-03-27 23:22:35 +0800

    Studied arithmetic operators in java

---
+++ src/Operators.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Operators {
+
+    public static void main (String[] args){
+
+        String item;
+        int quantity;
+        double price;
+        double total;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What do you want to buy? ");
+        item = scanner.nextLine();
+
+        System.out.print("How many do you want to buy? ");
+        quantity = scanner.nextInt();
+
+        System.out.print("How much is the item? ");
+        price = scanner.nextDouble();
+
+        total = price * quantity;
+
+        System.out.println("The total is: $" +total);
+
+        scanner.close();
+
+    }
+
+}
+
+
+//        System.out.print("Enter a number: ");
+//        x = scanner.nextInt();
+//
+//        System.out.print("Enter another number: ");
+//        y = scanner.nextInt();
+//
+//        z = x + y;
+//        System.out.print("The sum is: "+z);
